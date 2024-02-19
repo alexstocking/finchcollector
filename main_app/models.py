0c687db019efb885ae5633c9b7beb0e71b59d960
Alex <alexstocking@hotmail.co.uk>
2024-02-19 13:34:32 +0000

    Finch Collector Lab

--- main_app/models.py
+++ main_app/models.py
@@ -1,3 +1,56 @@
 from django.db import models
+from django.urls import reverse
+
+from datetime import date
 
 # Create your models here.
+
+MEALS = (
+    ('B', 'Breakfast'),
+    ('L', 'Lunch'),
+    ('D', 'Dinner')
+)
+
+class Toy(models.Model):
+  name = models.CharField(max_length=50)
+  color = models.CharField(max_length=20)
+
+  def __str__(self):
+    return self.name
+
+  def get_absolute_url(self):
+    return reverse('toys_detail', kwargs={'pk': self.id})
+
+class Finch(models.Model):
+    name_species = models.CharField(max_length=100)
+    scientific_name = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    habitat = models.CharField(max_length=250)
+    diet = models.CharField(max_length=250)
+    average_lifespan = models.CharField(max_length=50)
+    image_url = models.URLField()
+    toys = models.ManyToManyField(Toy)
+
+    def __str__(self):
+        return self.name_species
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'finch_id': self.id})
+    
+    def fed_for_today(self):
+        return self.feeding_set.filter(date=date.today()).count() >= len(MEALS)
+    
+class Feeding(models.Model):
+  date = models.DateField('Feeding date')
+  meal = models.CharField(
+      max_length=1,
+      choices=MEALS,
+      default=MEALS[0][0]
+    )
+  finch = models.ForeignKey(Finch, on_delete=models.CASCADE)
+  
+  def __str__(self):
+      return f"{self.get_meal_display()} on {self.date}"
+  
+  class Meta:
+      ordering = ['-date']
--- main_app/views.py
+++ main_app/views.py
@@ -1,43 +1,9 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
 
-finches = [
-    {
-        'species': 'Zebra Finch',
-        'scientific_name': 'Taeniopygia guttata',
-        'description': 'The zebra finch is a small, colorful bird native to Australia. They are popular as pets due to their cheerful disposition and melodious song. Zebra finches are highly social and often found in large flocks in the wild.',
-        'habitat': 'Grasslands, forests, and urban areas',
-        'diet': 'Mainly seeds, supplemented with insects and fruits',
-        'average_lifespan': '5 to 7 years',
-        'image_url': 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/123646541/1800'
-    },
-    {
-        'species': 'Gouldian Finch',
-        'scientific_name': 'Erythrura gouldiae',
-        'description': 'The Gouldian finch, also known as the rainbow finch, is a strikingly colored bird native to northern Australia. It is highly sought after by aviculturalists for its vibrant plumage, which comes in various color mutations.',
-        'habitat': 'Savannah woodlands and grasslands',
-        'diet': 'Mainly grass seeds, supplemented with insects',
-        'average_lifespan': '6 to 8 years',
-        'image_url': 'https://upload.wikimedia.org/wikipedia/commons/thumb/3/3e/Male_adult_Gouldian_Finch.jpg/640px-Male_adult_Gouldian_Finch.jpg'
-    },
-    {
-        'species': 'Society Finch',
-        'scientific_name': 'Lonchura domestica',
-        'description': 'The society finch, also known as the Bengalese finch, is a small, hardy bird that has been domesticated for centuries. It is a popular choice for novice bird keepers due to its ease of care and sociable nature.',
-        'habitat': 'Originally from Asia, now domesticated worldwide',
-        'diet': 'Seeds, greens, and occasional insects',
-        'average_lifespan': '5 to 7 years',
-        'image_url': 'https://www.serenityusa.com/app/uploads/2023/10/society-finch-perched-outside.jpg'
-    },
-    {
-        'species': 'Java Sparrow',
-        'scientific_name': 'Lonchura oryzivora',
-        'description': 'Although commonly referred to as a sparrow, the Java sparrow is actually a species of finch native to Java, Indonesia. It is known for its bold black-and-white plumage and distinctive pink bill.',
-        'habitat': 'Grasslands, agricultural fields, and urban areas',
-        'diet': 'Mainly seeds, with some greens and insects',
-        'average_lifespan': '5 to 8 years',
-        'image_url': 'https://cdn.download.ams.birds.cornell.edu/api/v1/asset/97645141/1800'
-    }
-]
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.views.generic import ListView, DetailView
+from .models import Finch, Toy
+from .forms import FeedingForm
 
 # Create your views here.
 def home(request):
@@ -47,6 +13,63 @@ def about(request):
   return render(request, 'about.html')
 
 def finches_index(request):
+  finches = Finch.objects.all()
   return render(request, 'finches/index.html', {
     'finches': finches
   })
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  id_list = finch.toys.all().values_list('id')
+  toys_finch_doesnt_have = Toy.objects.exclude(id__in=id_list)
+  feeding_form = FeedingForm()
+  return render(request, 'finches/detail.html', { 'finch': finch, 'feeding_form': feeding_form, 'toys': toys_finch_doesnt_have })
+
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = ['name_species', 'scientific_name', 'description', 'habitat', 'diet', 'average_lifespan', 'image_url']
+
+class FinchUpdate(UpdateView):
+  model = Finch
+  # Let's disallow the renaming of a cat by excluding the name field!
+  fields = ['description', 'habitat', 'diet', 'average_lifespan', 'image_url']
+
+class FinchDelete(DeleteView):
+  model = Finch
+  success_url = '/finches'
+
+def add_feeding(request, finch_id):
+  form = FeedingForm(request.POST)
+  if form.is_valid():
+    new_feeding = form.save(commit=False)
+    new_feeding.finch_id = finch_id
+    new_feeding.save()
+  return redirect('detail', finch_id=finch_id)
+
+class ToyList(ListView):
+  model = Toy
+
+class ToyDetail(DetailView):
+  model = Toy
+
+class ToyCreate(CreateView):
+  model = Toy
+  fields = '__all__'
+
+class ToyUpdate(UpdateView):
+  model = Toy
+  fields = ['name', 'color']
+
+class ToyDelete(DeleteView):
+  model = Toy
+  success_url = '/toys'
+
+def assoc_toy(request, finch_id, toy_id):
+  # Note that you can pass a toy's id instead of the whole toy object
+  Finch.objects.get(id=finch_id).toys.add(toy_id)
+  return redirect('detail', finch_id=finch_id)
+
+def remove_toy(request, finch_id, toy_id):
+  Finch.objects.get(id=finch_id).toys.remove(toy_id)
+  return redirect('detail', finch_id=finch_id)
